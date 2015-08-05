817198d307308c562e7597716386cd36be767958
Matthew X. Economou <xenophon@irtnog.org>
2015-08-05 06:57:11 -0400

    fix(_modules): import `salt.utils.dictupdate` and use it for selected dictionary merges
    The function `salt.utils.dictupdate.update()` isn't imported into
    `__salt__`, so it isn't possible to use that function to recursively
    merge two dictionaries without monkey patching Salt, hence the `irtnog`
    module.

---
+++ _modules/irtnog.py
@@ -0,0 +1,20 @@
+#### _MODULES/IRTNOG.PY --- Convenience library
+
+### Unfortunately, several useful functions aren't accessible from
+### Jinja, whether natively or via __salt__.  This module provides a
+### wrapper for those functions we need to selectively import, like
+### salt.utils.dictupdate.update().
+
+import salt.utils.dictupdate
+
+def __virtual__():
+    return 'irtnog'
+
+def dictupdate(dest, upd):
+    'Recursively merge dictionaries
+
+Contrast with Python\'s dict.update() method, which only performs a
+shallow merge.'
+    return salt.utils.dictupdate.update(dest, upd)
+
+#### _MODULES/IRTNOG.PY ends here.
