35aea2088501541a03dbfbd6edfdffd9807f23a0
Matthew X. Economou <xenophon@irtnog.org>
2015-08-10 09:32:29 -0400

    refactor(_modules): remove unnecessary indirection

--- _modules/irtnog.py
+++ _modules/irtnog.py
@@ -5,17 +5,9 @@
 ### wrapper for those functions we need to selectively import, like
 ### salt.utils.dictupdate.update().
 
-import salt.utils.dictupdate
+from salt.utils.dictupdate import update as dictupdate
 
 def __virtual__():
     return 'irtnog'
-
-def dictupdate(dest, upd):
-    '''Recursively merge dictionaries
-
-    Contrast with Python\'s dict.update() method, which only performs
-    a shallow merge.
-    '''
-    return salt.utils.dictupdate.update(dest, upd)
 
 #### _MODULES/IRTNOG.PY ends here.
