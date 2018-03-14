f77874d2f8c651c5d907036dc74f674475688fb6
Matthew X. Economou <xenophon@irtnog.org>
2018-03-13 23:52:48 -0400

    feat(salt.modules.irtnog_ec2): rewrite to use boto3

--- _modules/irtnog_ec2.py
+++ _modules/irtnog_ec2.py
@@ -1,70 +1,53 @@
 # Import Python libs
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function, unicode_literals
 import logging
-import time
-import json
 
 # Import Salt libs
-import salt.utils
-import salt.utils.compat
-import salt.ext.six as six
-from salt.exceptions import SaltInvocationError, CommandExecutionError
-from salt.utils.versions import LooseVersion as _LooseVersion
-
-# Import third party libs
-try:
-    # pylint: disable=unused-import
-    import boto
-    import boto.ec2
-    # pylint: enable=unused-import
-    from boto.ec2.blockdevicemapping import BlockDeviceMapping, BlockDeviceType
-    HAS_BOTO = True
-except ImportError:
-    HAS_BOTO = False
+import salt.utils.versions
 
 log = logging.getLogger(__name__)
 
+# Import third party libs
+#pylint: disable=unused-import
+try:
+    import botocore
+    import boto3
+    import jmespath
+    logging.getLogger('boto3').setLevel(logging.CRITICAL)
+    HAS_BOTO = True
+except ImportError:
+    HAS_BOTO = False
+#pylint: enable=unused-import
+
+
 def __virtual__():
     '''
-    Only load if boto libraries exist and if boto libraries are greater than
-    a given version.
+    Only load if boto libraries exist.
     '''
-    required_boto_version = '2.8.0'
-    # the irtnog_ec2 execution module relies on the connect_to_region() method
-    # which was added in boto 2.8.0
-    # https://github.com/boto/boto/commit/33ac26b416fbb48a60602542b4ce15dcc7029f12
-    if not HAS_BOTO:
-        return (False, "The irtnog_ec2 module cannot be loaded: boto library not found")
-    elif _LooseVersion(boto.__version__) < _LooseVersion(required_boto_version):
-        return (False, "The irtnog_ec2 module cannot be loaded: boto library version incorrect ")
-    else:
-        __utils__['boto.assign_funcs'](__name__, 'ec2', pack=__salt__)
-        return True
+    has_boto_reqs = salt.utils.versions.check_boto_reqs()
+    if has_boto_reqs is True:
+        __ utils__['boto3.assign_funcs'](__name__, 'ec2')
+    return has_boto_reqs
 
-def find_images(filters={}, region=None, key=None, keyid=None, profile=None, return_objs=False):
+
+def find_images(region=None, key=None, keyid=None, profile=None, return_objs=False, **kwargs):
     conn = _get_conn(region=region, key=key, keyid=keyid, profile=profile)
-    try:
-        images = conn.get_all_images(filters=filters)
-        log.debug('The filters criteria {0} matched the following '
-                  'images: {1}.'.format(filters, images))
-        if images:
-            if return_objs:
-                return images
-            return [image.id for image in images]
-        else:
-            return False
-    except boto.exception.BotoServerError as e:
-        log.error(e)
-        return False
+    resp = conn.describe_images(**kwargs)
+    log.debug('The filters criteria {0} matched the following '
+              'images: {1}.'.format(kwargs, resp))
+    if return_objs:
+        return resp['Images']
+    return [image.id for image in resp['Images']]
 
-def find_latest_image(filters={}, region=None, key=None, keyid=None, profile=None, return_obj=False):
-    images = find_images(filters=filters, region=region, key=key, keyid=keyid, profile=profile, return_objs=True)
+
+def find_latest_image(region=None, key=None, keyid=None, profile=None, return_obj=False, **kwargs):
+    images = find_images(region=region, key=key, keyid=keyid, profile=profile, return_objs=True, **kwargs)
     if images:
         images.sort(reverse=True, key=lambda image: image.creation_date)
         image = images[0]
         log.debug('This identified {0} with a creation date of {1} '
                   'as the latest image matching the filter criteria '
-                  '{2}.'.format(image.id, image.creation_date, filters))
+                  '{2}.'.format(image.id, image.creation_date, kwargs))
         if return_obj:
             return image
         return image.id
